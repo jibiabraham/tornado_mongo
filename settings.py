83d2cfd5d5aaa7177371791d957f751a666a1952
root <root@Jibi.(none)>
2013-01-14 16:16:02 +0530

    cleanup

--- settings.py
+++ settings.py
@@ -12,10 +12,6 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 
 define("port", default=8888, help="run on the given port", type=int)
 define("debug", default=False, help="debug mode")
-define("mysql_host", default="127.0.0.1:3306", help="blog database host")
-define("mysql_database", default="blog", help="blog database name")
-define("mysql_user", default="blog", help="blog database user")
-define("mysql_password", default="blog", help="blog database password")
 
 tornado.options.parse_command_line()
 
